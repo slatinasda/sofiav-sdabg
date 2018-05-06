705e2ed99451bb70b364495e34176833cd3a779e
Slatina <slatina.church@gmail.com>
2018-05-06 13:54:41 +0300

    Initial commit after integrating theme Hide main section description for XS and SM devices Add Live section with Youtube channel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Application modules
+import { AppRoutingModule } from './app-routing.module';
+import { HomeModule } from './home/home.module';
+
+// App is our top-level component
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HomeModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
