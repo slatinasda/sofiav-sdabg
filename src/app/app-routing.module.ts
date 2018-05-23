b2f294792a26ac35100da70992a594a376389446
Slatina <slatina.church@gmail.com>
2018-05-23 20:42:04 +0300

    Set HomeComponent in app-routing.module.ts, which will save the unnecessary redirect to /home Enable vendor chuck which will utilize browser caching - the application logic is more likely to change, than the third-party dependencies.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
 
 const appRoutes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '', component: HomeComponent, pathMatch: 'full' },
   { path: '**', component: NotFoundComponent },
 ];
 
