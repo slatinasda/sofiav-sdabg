9809079ce377e0ae79d9c62703298c0087ee5c58
Slatina <slatina.church@gmail.com>
2018-07-14 21:17:44 +0300

    Set page title in BeliefsComponent

--- src/app/beliefs/beliefs.component.ts
+++ src/app/beliefs/beliefs.component.ts
@@ -1,8 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-beliefs',
   templateUrl: './beliefs.component.html',
   styleUrls: ['./beliefs.component.scss']
 })
-export class BeliefsComponent { }
+export class BeliefsComponent {
+  constructor(private titleService: Title) {
+    // TODO Create custom title service which is going to add site name automatically
+    this.titleService.setTitle('Вярвания | Църква на Адвентистите от Седмия Ден - София "В" Слатина');
+  }
+}
