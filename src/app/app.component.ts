2922b2fb5b3a5586d1c316dc34ebbef97a43ea9c
Slatina <slatina.church@gmail.com>
2024-01-13 19:25:06 +0200

    Automatically hide mobile menu when user clicks on a navigation item

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  @ViewChild('navbarMain') navbarMain: ElementRef | null = null;
+
   today = Date.now();
+
+  hideMobileMenu() {
+    this.navbarMain?.nativeElement.classList.remove('show');
+  }
 }
