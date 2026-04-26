5a79a24a6bde0e3b0381ff9b525bf69318ceaa9c
Slatina <slatina.church@gmail.com>
2026-04-26 23:23:51 +0300

    When the modal is open, add a URL fragment and push to history so we close the modal on history back.
    Example:
     - User clicks a bible verse
     - The modal is open
     - The URL is updated to /sabbath-school/2026-02/lessons/05/01#bible
     - User clicks browser back button
     - Modal is closed and #bible is remove from the URL

--- src/app/shared/components/modal/modal.component.ts
+++ src/app/shared/components/modal/modal.component.ts
@@ -1,33 +1,84 @@
-import { Component, Input, Output, EventEmitter, HostListener } from '@angular/core';
+import { Component, Input, Output, EventEmitter, HostListener, OnInit, OnDestroy, NgZone } from '@angular/core';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.scss']
 })
-export class ModalComponent {
-  @Input() open = false;
+export class ModalComponent implements OnInit, OnDestroy {
   @Input() title = '';
   @Input() size: 'default' | 'lg' | 'sm' = 'default';
+  @Input() urlFragment = 'popup';
   @Output() openChange = new EventEmitter<boolean>();
   @Output() closeEvent = new EventEmitter<void>();
 
+  private _open = false;
+
+  constructor(private zone: NgZone) {}
+
+  @Input()
+  set open(value: boolean) {
+    if (value && !this._open) {
+      this.addFragment();
+    }
+    this._open = value;
+  }
+
+  get open(): boolean {
+    return this._open;
+  }
+
+  ngOnInit() {
+    if (this.urlFragment) {
+      // Listen for browser back/forward navigation
+      // When the user presses back and the #fragment is removed, close the modal
+      window.addEventListener('popstate', this.onPopState);
+    }
+  }
+
+  ngOnDestroy() {
+    window.removeEventListener('popstate', this.onPopState);
+  }
+
+  private onPopState = (): void => {
+    if (this._open) {
+      this.zone.run(() => {
+        this._open = false;
+        this.openChange.emit(false);
+        this.closeEvent.emit();
+      });
+    }
+  };
+
   @HostListener('document:keydown.escape', ['$event'])
   onKeydownHandler(event: KeyboardEvent): void {
-    if (this.open) {
-      this.close();
+    if (this._open) {
+      this.goBack();
     }
   }
 
   onBackdropClick(event: MouseEvent): void {
     if ((event.target as HTMLElement).classList.contains('modal')) {
-      this.close();
+      this.goBack();
     }
   }
 
-  close(): void {
-    this.open = false;
-    this.openChange.emit(false);
-    this.closeEvent.emit();
+  /**
+   * Closes the modal by triggering the browser back button.
+   */
+  goBack(): void {
+    if (this._open) {
+      window.history.back();
+    }
+  }
+
+  private addFragment(): void {
+    if (this.urlFragment) {
+      const path = window.location.pathname + window.location.search;
+      const fragment = '#' + this.urlFragment;
+      if (!path.endsWith(fragment)) {
+        window.history.pushState(null, '', path + fragment);
+      }
+    }
   }
 }
