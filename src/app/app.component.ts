daf4f9bca0daad501d8b2ceb05e3d2c2e98a3d6c
Slatina <slatina.church@gmail.com>
2026-07-19 19:39:42 +0300

    Disable CSS critical-path inlining and remove FOUC fade-in workaround

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Renderer2 } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { NgbCollapse } from '@ng-bootstrap/ng-bootstrap';
@@ -11,25 +11,14 @@ import { CurrentQuarterService } from './sabbath-school/current-quarter.service'
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   mobileMenuOpen = false;
 
   today = Date.now();
   sabbathSchoolLink: string;
 
-  constructor(
-    private quarterService: CurrentQuarterService,
-    private renderer: Renderer2,
-  ) {
+  constructor(private quarterService: CurrentQuarterService) {
     this.sabbathSchoolLink = `/sabbath-school/${this.quarterService.yearAndQuarter()}`;
-  }
-
-  ngOnInit() {
-    setTimeout(() => {
-      const appRoot = this.renderer.selectRootElement('app-root', true);
-      this.renderer.removeClass(appRoot, 'app-root--loading');
-      this.renderer.addClass(appRoot, 'app-root--ready');
-    }, 500);
   }
 
   hideMobileMenu() {
